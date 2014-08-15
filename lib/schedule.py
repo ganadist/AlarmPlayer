b94fc7e79a702d4c9beb136fd1fc662c9a9895bb
Young-Ho Cha <ganadist@gmail.com>
2014-08-15 22:31:00 +0900

    add WeeklyScheduler
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- lib/schedule.py
+++ lib/schedule.py
@@ -4,23 +4,28 @@ from eloop import Timer, main
 def delta_to_ms(td):
     return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**3
 
-class DailyScheduler:
-    def __init__(self, time, cb, *args):
-        self.date = datetime.date.today()
-        self.time = time
-        self.datetime = datetime.datetime.combine(self.date, self.time)
+class BaseScheduler:
+    def __init__(self, cb, *args):
         self.cb = cb
         self.cb_args = args
 
+    def setup(self, date, time):
+        self.date = date
+        self.time = time
+        schedule_datetime = datetime.datetime.combine(self.date, self.time)
+
         now = datetime.datetime.now()
-        if now > self.datetime:
-            self.date = self.date + datetime.timedelta(1)
+        if now > schedule_datetime:
+            self.update_next_date()
 
         self.do_schedule()
 
+    def update_next_date(self):
+        raise NotImplementedError
+
     def run_callback(self):
         self.cb(*self.cb_args)
-        self.date = self.date + datetime.timedelta(1)
+        self.update_next_date()
         self.do_schedule()
 
     def do_schedule(self):
@@ -28,6 +33,33 @@ class DailyScheduler:
         now = datetime.datetime.now()
         delta = schedule_datetime - now
         self.tag = Timer(delta_to_ms(delta), self.run_callback)
+
+
+class WeeklyScheduler(BaseScheduler):
+    def __init__(self, weekday, time, cb, *args):
+        # weekday 0: sunday
+        # weekday 6: saturday
+        BaseScheduler.__init__(self, cb, *args)
+
+        # convert python weekday
+        weekday -= 1
+        weekday %= 7
+
+        date = datetime.date.today()
+        date += datetime.timedelta((weekday - date.weekday())%7)
+        self.setup(date, time)
+
+    def update_next_date(self):
+        self.date = self.date + datetime.timedelta(7)
+
+class DailyScheduler(BaseScheduler):
+    def __init__(self, time, cb, *args):
+        BaseScheduler.__init__(self, cb, *args)
+        date = datetime.date.today()
+        self.setup(date, time)
+
+    def update_next_date(self):
+        self.date = self.date + datetime.timedelta(1)
 
 class OneshotScheduler:
     def __init__(self, timestamp, cb, *args):
@@ -48,6 +80,7 @@ if __name__ == '__main__':
         print datetime.datetime.now()
         print 'hh', args
         return True
-    time = datetime.time(10, 51, 10)
-    d = DailyScheduler(time, cb, 'timer', 1, 2)
+    time = datetime.time(22, 51, 10)
+    #d = DailyScheduler(time, cb, 'timer', 1, 2)
+    d = WeeklyScheduler(6, time, cb, 'timer', 1, 2)
     main()
